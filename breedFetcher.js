04010d5f15d9b97f9ad2b23ca1e5051268990c3d
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-18 11:44:36 -0400

    added breedfetcher and gitignoe files

---
+++ breedFetcher.js
@@ -0,0 +1,25 @@
+"use strict;";
+const request = require("request");
+
+const fetchBreedDescription = function (breedName) {
+  url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  ///console.log(url);
+  request(url, (error, resp, body) => {
+    if (error) {
+      console.log(`Error occured in fetching, details:- ${error}`);
+    }
+    //console.log(typeof body);
+    //console.log(resp);
+    const data = JSON.parse(body);
+    // console.log(data);
+    //console.log(typeof data);
+    const breed = data[0];
+    console.log(`Description: ${breed.description}`);
+    if (breed) {
+      console.log(`Description: ${breed.description}`);
+    } else {
+      console.log(`Failed to find breed ${breedName}`, null);
+    }
+  });
+};
+fetchBreedDescription("Chartreux");
